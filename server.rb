925f92fd8062ade3cf98145fcdfc23db80813c13
takuma104 <takuma104@0af54733-fd48-0410-9cb7-ad015ecf8501>
2008-05-06 17:19:30 +0000

    copy r39 banches/rtpbroadcaster

--- server.rb
+++ server.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby -wKU
 #
-#    RubyIZUMI Ver.0.10
+#    RubyIZUMI Ver.0.20
 #
 #    Copyright (C) 2008 Takuma Mori, SGRA Corporation
 #    <mori@sgra.co.jp> <http://www.sgra.co.jp/en/>
@@ -21,6 +21,9 @@
 
 $: << "./lib" #!!!
 $: << "./lib/hmac" #!!!
+$: << "./lib/rtmp" #!!!
+$: << "./lib/mp4" #!!!
+$: << "./lib/rtp" #!!!
 
 require 'socket'
 require 'rtmp_session'
@@ -32,26 +35,28 @@ require 'utils'
 require 'openssl'
 
 module RTMP
-  FmsVer = 'RubyIZUMI/0,1,1,0'
+  FmsVer = 'RubyIZUMI/0,2,0,0'
 end
 
 def usage
   puts "Usage: server.rb (options) document_root_directory or filename"
   puts "Options: -p listen port (default=1935)"
-  puts "         -v verbose (0:debug 1:info) (default=1)"
+  puts "         -v verbose"
   puts "         -l logfile (default=STDERR)"
   puts "         -d (daemonize)"
+  puts "         -rtp rtpmode"
   exit(-1)
 end
 
 def parse_argv
-  options = {:p=>1935,:v=>1,:log=>nil,:d=>false}
+  options = {:p=>1935,:v=>false,:log=>nil,:d=>false,:rtp=>false}
 
   opt = OptionParser.new
   opt.on('-p VAL') {|v| options[:p] = v.to_i }
-  opt.on('-v VAL') {|v| options[:v] = v.to_i }
   opt.on('-l VAL') {|v| options[:l] = v }
+  opt.on('-v') {|v| options[:v] = true }
   opt.on('-d') {|v| options[:d] = true }
+  opt.on('-rtp') {|v| options[:rtp] = true }
   opt.parse!(ARGV)
 
   if ARGV.size != 1
@@ -93,14 +98,19 @@ def daemon
   exit! 0
 end
 
-def server_loop(path, port)
-  pool = IZUMI::StreamPool.new(path)
-
-  case path.type
-  when :directory
-    IzumiLogger.info "Document Root:#{path.path}"
-  when :file
-    IzumiLogger.info "Target File:#{path.path}"
+def server_loop(path, port, rtpmode)
+  pool = nil
+  if rtpmode
+    pool = IZUMI::StreamPoolRTP.new(path)
+    IzumiLogger.info "RTP Mode SDP:#{path.path}"
+  else
+    pool = IZUMI::StreamPool.new(path)
+    case path.type
+    when :directory
+      IzumiLogger.info "Document Root:#{path.path}"
+    when :file
+      IzumiLogger.info "Target File:#{path.path}"
+    end
   end
 
   # disable DNS reverse lookup
@@ -133,14 +143,14 @@ if $0 == __FILE__
   
   # setup logger 
   IzumiLogger = Logger.new(if options[:l] then options[:l] else STDERR end)
-  IzumiLogger.level = if options[:v] == 1 then Logger::INFO else Logger::DEBUG end  
+  IzumiLogger.level = if options[:v] then Logger::DEBUG else Logger::INFO end  
   
   if options[:d]
     puts "Warnning: Please specify log file in daemon mode." unless options[:l]
     daemon do
-      server_loop(path, options[:p])
+      server_loop(path, options[:p], options[:rtp])
     end
   else
-    server_loop(path, options[:p])
+    server_loop(path, options[:p], options[:rtp])
   end
 end
